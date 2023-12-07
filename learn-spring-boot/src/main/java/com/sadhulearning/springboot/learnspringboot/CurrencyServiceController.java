faf824abe3498c799cf07de1cb38206660a802fd
drumil <akheniad@gmail.com>
2023-12-07 18:06:36 +0530

    learn-spring-boot :- configureProperties

---
+++ learn-spring-boot/src/main/java/com/sadhulearning/springboot/learnspringboot/CurrencyServiceController.java
@@ -0,0 +1,20 @@
+package com.sadhulearning.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyServiceController {
+	
+	@Autowired
+	private CurrencyService currencyService;
+	
+	@RequestMapping("/currency-service")
+	public CurrencyService retriveAllDataOfCurrencyService(){
+		return currencyService;
+	}
+}
